b2e5748e7d4246960c37deea2bbe4b716e0cc3e6
ㄷㅌ <62054524+exte0427@users.noreply.github.com>
2022-02-24 15:32:24 +0900

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "html.h"
+using namespace html;
+
+int main() {
+	string code = "<h1 a=\"a\"><div>hi</div></hi><bye c=\"d\" asd=\"sad\">j</bye>";
+
+	setting set;
+	set.domMaker = "Var.dom";
+	set.stateMaker = "Var.state";
+	set.parentTag = "jsx";
+
+	cout << jsx::make(code,set) << endl;
+
+	return 0;
+}
